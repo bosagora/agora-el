1db0783f41c27f8bab0ba8711cb3f93e82d35c98
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2022-05-26 15:43:17 +0300

    Pay 30% of tx fees to commons budget

--- core/state_transition.go
+++ core/state_transition.go
@@ -344,7 +344,15 @@ func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
 	if rules.IsLondon {
 		effectiveTip = cmath.BigMin(st.gasTipCap, new(big.Int).Sub(st.gasFeeCap, st.evm.Context.BaseFee))
 	}
-	st.state.AddBalance(st.evm.Context.Coinbase, new(big.Int).Mul(new(big.Int).SetUint64(st.gasUsed()), effectiveTip))
+	txFee := new(big.Int).Mul(new(big.Int).SetUint64(st.gasUsed()), effectiveTip)
+
+	if st.evm.ChainConfig().IsCommonsBudgetActivated(st.evm.Context.BlockNumber) {
+		commonsCut := new(big.Int).Div(new(big.Int).Mul(big.NewInt(30), txFee), big.NewInt(100)) // %30 of txFee
+		st.state.AddBalance(st.evm.Context.Coinbase, txFee.Sub(txFee, commonsCut)) // total - commons cut
+		st.state.AddBalance(st.evm.ChainConfig().CommonsBudget, commonsCut) // commons cut
+	} else {
+		st.state.AddBalance(st.evm.Context.Coinbase, txFee)
+	}
 
 	return &ExecutionResult{
 		UsedGas:    st.gasUsed(),
